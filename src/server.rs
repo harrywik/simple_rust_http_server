dfa1ef912f109f4977219214d8ec2cd4c50573da
Harry Wik <harrywiksmail@gmail.com>
2022-09-03 14:31:21 +0200

    reading from TcpStream

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::net::TcpListener;
+use std::io::Read;
+
 pub struct Server {
     addr: String,
 }
@@ -11,5 +14,20 @@ impl Server {
 
     pub fn run(self) -> () {
         println!("server started on: {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+            Ok((mut stream, client)) => {
+                println!("socket: {:?}\nclient: {:?}", stream, client);
+                let mut buffer: [u8; 1024] = [0; 1024];
+                match stream.read(&mut buffer) {
+                    Ok(_) => println!("received request: {}", String::from_utf8_lossy(&buffer)),
+                    Err(e) => println!("failed to read from TcpStream: {:?}", e)
+
+                }
+            },
+            Err(e) => println!("failed to establish a connection: {:?}", e),
+            }
+        }
     }
 }
