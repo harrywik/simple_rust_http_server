7a3e3e9ac61f05d66734a0c126264b62578b5c52
Harry Wik <harrywiksmail@gmail.com>
2022-09-03 22:58:23 +0200

    conversion from buffer to Request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,54 @@
 use super::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::{fmt, str};
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        let req_str = str::from_utf8(buffer)?;
+
+        unimplemented!();
+    }
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidMethod => "invalid method, please see Method enum in http/method.rs",
+            Self::InvalidProtocol => "only HTTP/1.1 is supported",
+            Self::InvalidEncoding => "only utf8 encoding accepted",
+            Self::InvalidRequest => "invalid request" 
+        }
+    }
+}
+
+impl Error for ParseError {}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message())
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use crate::http::Request;
 use std::net::TcpListener;
 use std::io::Read;
 
@@ -15,18 +16,26 @@ impl Server {
     pub fn run(self) -> () {
         println!("server started on: {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
+
         loop {
             match listener.accept() {
             Ok((mut stream, client)) => {
                 println!("socket: {:?}\nclient: {:?}", stream, client);
                 let mut buffer: [u8; 1024] = [0; 1024];
-                match stream.read(&mut buffer) {
-                    Ok(_) => println!("received request: {}", String::from_utf8_lossy(&buffer)),
-                    Err(e) => println!("failed to read from TcpStream: {:?}", e)
 
+                match stream.read(&mut buffer) {
+                    Ok(_) => {
+                        println!("received request: {}", String::from_utf8_lossy(&buffer));
+                        match Request::try_from(&buffer as &[u8]) {
+                            Ok(req) => {},
+                            Err(e) => println!("could not convert buffer into Request. Error: {}", e)
+                        }
+                    }
+
+                    Err(e) => println!("failed to read from TcpStream: {}", e)
                 }
             },
-            Err(e) => println!("failed to establish a connection: {:?}", e),
+            Err(e) => println!("failed to establish a connection: {}", e),
             }
         }
     }
