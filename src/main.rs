e09bc8f39036e6fbcd15cbbb546e6984693f017b
Harry Wik <harrywiksmail@gmail.com>
2022-09-03 12:59:05 +0200

    organized into modules

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
+use server::Server;
+use http::Method;
+use http::Request;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) -> () {
-        println!("server started on: {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
