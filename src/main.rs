263aa280ad5014b63e41cc66c24f67fe68903774
Harry Wik <harrywiksmail@gmail.com>
2022-09-04 21:58:16 +0200

    read files from ./public/

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ mod http;
 mod website_handler;
 
 fn main() {
+
     env::set_var("RUST_BACKTRACE", "1");
+    let public_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(&mut WebsiteHandler);
+    server.run(&mut WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,19 +1,32 @@
+use std::fs::read_to_string;
 use crate::server::Handler;
 use crate::http::{Method, Request, Response, StatusCode};
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, req: &Request) -> Response {
         match req.method() {
-            Method::GET => match req.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Main Page</h1>".to_string())),
-                "/greet" => {
-                    Response::new(StatusCode::OK, Some("<h1>Hello!</h1>".to_string()))
-                },
-                _ => Response::new(StatusCode::NotFound, None)
+            Method::GET => match self.read_file(req.path()) {
+                Some(file_string) => Response::new(StatusCode::OK, Some(file_string)),
+                None => Response::new(StatusCode::NotFound, None)
             },
             _ => Response::new(StatusCode::BadRequest, None)
         }
+    }
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {
+            public_path
+        }
+    }
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        read_to_string(path).ok()
     }
 }
